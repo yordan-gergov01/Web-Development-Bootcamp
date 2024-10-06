9335c5e40db7980711d7b735485339981ce56152
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-06 07:43:16 +0300

    Add files via upload

---
+++ Exercises/React practice/315-es6-spread-operator/src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+createRoot(document.getElementById('root')).render(
+    <App />
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
